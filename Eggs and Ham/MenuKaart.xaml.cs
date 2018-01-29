3bf2c8f090665694ba258d9597806f6f091b0ebd
Stefan Beckers <stefan.beckers206490@student.leeuwenborgh.nl>
2018-01-29 10:48:47 +0100

    menukaart clear

--- Eggs and Ham/MenuKaart.xaml.cs
+++ Eggs and Ham/MenuKaart.xaml.cs
@@ -20,73 +20,9 @@ namespace Eggs_and_Ham
     /// </summary>
     public partial class MenuKaart : Page
     {
-        public static ListBox MenuKaartLB = new ListBox();
-        public static StackPanel terugPanel = new StackPanel();
-        public static Button terugknop = new Button();
-
-        public static Button invoeroke = new Button();
-        public static TextBox invoerGroeptxt = new TextBox();
         public MenuKaart()
         {
             InitializeComponent();
-            //terugknop
-            terugknop.Width = 50;
-            terugknop.Content = "terug";
-            terugknop.Click += Terug;
-
-            //button invoeroke
-
-            invoeroke.Margin = new Thickness(0, 0, 0, 0);
-            invoeroke.Width = 50;
-            invoeroke.Content = "Add";
-            invoeroke.Click += MenuItem;
-
-            //de textbox
-            invoerGroeptxt.Margin = new Thickness(0, 0, 0, 0);
-            invoerGroeptxt.Width = 150;
-
-
-            //stackpanel properties
-            terugPanel.Orientation = Orientation.Horizontal;
-            terugPanel.HorizontalAlignment = HorizontalAlignment.Right;
-            terugPanel.VerticalAlignment = VerticalAlignment.Bottom;
-            terugPanel.Margin = new Thickness(0, 0, 10, 10);
-            Menu_kaart.Children.Add(terugPanel);
-            terugPanel.Children.Add(terugknop);
-            terugPanel.Children.Add(invoerGroeptxt);
-            terugPanel.Children.Add(invoeroke);
-
-            
-
-            MenuKaartLB.Items.Add("Menu Kaart");
-            MenuKaartLB.Items.Add("Eggs");
-            MenuKaartLB.HorizontalAlignment = HorizontalAlignment.Left;
-            MenuKaartLB.VerticalAlignment = VerticalAlignment.Top;
-            MenuKaartLB.Margin = new Thickness(10, 10, 0, 0);
-            Menu_kaart.Children.Add(MenuKaartLB);
-            MenuKaartLB.SelectionChanged += AddGerecht;
-
-        }
-
-        public void Terug(object sender, RoutedEventArgs e)
-        {
-            MainWindow.MainMenu.Navigate(new HoofdMenu());
-        }
-
-        public void MenuItem(object sender, RoutedEventArgs e)
-        {
-            //een item toevoegen aan de menukaart.
-            MenuKaartLB.Items.Add(invoerGroeptxt.Text);
-            invoerGroeptxt.Text = "";
-        }
-
-        public void AddGerecht(object sender, SelectionChangedEventArgs e)
-        {
-            if (((ListBox)sender).SelectedItem.ToString() != "Menu Kaart")
-            {
-                //Reserveringen.strResVan
-                
-            }
         }
     }
 }
