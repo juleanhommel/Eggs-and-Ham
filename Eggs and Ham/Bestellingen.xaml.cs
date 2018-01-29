b63c35f11f73cfc265d5021d50f6ccd572c81a5b
Julean Hommel <julean.hommel204879@student.leeuwenborgh.nl>
2018-01-29 09:30:56 +0100

    weer dingen weg gehaald

--- Eggs and Ham/Bestellingen.xaml.cs
+++ Eggs and Ham/Bestellingen.xaml.cs
@@ -48,44 +48,10 @@ namespace Eggs_and_Ham
 
         public Bestellingen()
         {
-
-            //properties stackpanel
-            Back.Orientation = Orientation.Horizontal;
-            Back.HorizontalAlignment = HorizontalAlignment.Right;
-            Back.VerticalAlignment = VerticalAlignment.Bottom;
-            Back.Margin = new Thickness(0, 0, 10, 10);
-            BestelWindow.Children.Add(Back);
-            Back.Children.Add(Terug);
-
             //properties terug button
             Terug.Width = 50;
             Terug.Content = "terug";
             Terug.Click += MainMenu;
-
-            if (FirstLoad == true)
-            {
-                Tafel_1.Items.Add("tafel 1");
-                Tafel_2.Items.Add("Tafel 2");
-                Tafel_3.Items.Add("Tafel 3");
-                Tafel_4.Items.Add("Tafel 4");
-                Tafel_5.Items.Add("Tafel 5");
-                Tafel_6.Items.Add("Tafel 6");
-                Tafel_7.Items.Add("Tafel 7");
-                Tafel_8.Items.Add("Tafel 8");
-                Tafel_9.Items.Add("Tafel 9");
-                Tafel_10.Items.Add("Tafel 10");
-                Tafel_11.Items.Add("Tafel 11");
-                Tafel_12.Items.Add("Tafel 12");
-                Tafel_13.Items.Add("Tafel 13");
-                Tafel_14.Items.Add("Tafel 14");
-                Tafel_15.Items.Add("Tafel 15");
-                Tafel_16.Items.Add("Tafel 16");
-                Tafel_17.Items.Add("Tafel 17");
-                Tafel_18.Items.Add("Tafel 18");
-                Tafel_19.Items.Add("Tafel 19");
-                Tafel_20.Items.Add("Tafel 20");
-                FirstLoad = false;
-            }
         }
         public void MainMenu(object sender, RoutedEventArgs e)
         {
